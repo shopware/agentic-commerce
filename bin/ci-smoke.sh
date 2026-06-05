bf434c9926db8a363d1fb55f5d8d888f97469bc3
BrocksiNet <b.meyer@shopware.com>
2026-06-05 20:46:29 +0200

    fix(ci): load plugin autoload in zip smoke

--- bin/ci-smoke.sh
+++ bin/ci-smoke.sh
@@ -515,7 +515,7 @@ web php /var/www/html/bin/console system:config:set SwagAgenticCommerce.config.w
 web php /var/www/html/bin/console system:config:set SwagAgenticCommerce.config.continueUrlTemplate "${BASE_URL}/checkout/confirm?checkoutId={checkoutId}" --salesChannelId="${sales_channel_id}"
 
 store_api_mcp_available="$(web php -r 'require "/var/www/html/vendor/autoload.php"; echo class_exists("Shopware\\Core\\Framework\\Mcp\\Controller\\StoreApiMcpServerController") ? "1" : "0";')"
-core_agentic_files_available="$(web php -r 'require "/var/www/html/vendor/autoload.php"; echo Swag\AgenticCommerce\AgenticFiles\CoreSalesChannelFileFeature::isAvailableByClass() ? "1" : "0";')"
+core_agentic_files_available="$(web php -r 'require "/var/www/html/vendor/autoload.php"; $pluginAutoload = "/var/www/html/custom/plugins/SwagAgenticCommerce/vendor/autoload.php"; if (is_file($pluginAutoload)) { require_once $pluginAutoload; } echo (class_exists("Swag\\AgenticCommerce\\AgenticFiles\\CoreSalesChannelFileFeature") && Swag\AgenticCommerce\AgenticFiles\CoreSalesChannelFileFeature::isAvailableByClass()) ? "1" : "0";')"
 enabled_transports='["rest","a2a","embedded"]'
 expected_transports_json='["a2a","embedded","rest"]'
 if [[ "${store_api_mcp_available}" == "1" ]]; then
