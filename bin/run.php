f063ff49354a3ab0722b5cccc3e11bb105a7b1a9
Jonas Elfering <j.elfering@shopware.com>
2026-09-08 13:33:05 +0200

    fix(ci): load PHPStan Symfony extension

--- bin/run.php
+++ bin/run.php
@@ -136,6 +136,7 @@ function renderPhpstanConfig(string $pluginDir): string
     $rendered = strtr($template, [
         '__SHOPWARE_CORE_DIR__' => $coreDir,
         '__SHOPWARE_PHPSTAN_INCLUDES__' => renderShopwarePhpstanIncludes($coreDir),
+        '__PHPSTAN_SYMFONY_INCLUDE__' => renderPhpstanSymfonyInclude($coreDir),
         '__FUTURE_COMPATIBILITY_INCLUDE__' => renderFutureCompatibilityInclude($pluginDir, $coreDir),
         '__SHOPWARE_PHPSTAN_PARAMETERS__' => renderShopwarePhpstanParameters($coreDir),
         '__SHOPWARE_UNEXPECTED_TEST_COVERS_IGNORE__' => renderUnexpectedTestCoversIgnore($coreDir),
@@ -215,6 +216,13 @@ function renderFutureCompatibilityInclude(string $pluginDir, string $coreDir): s
     }
 
     return '';
+}
+
+function renderPhpstanSymfonyInclude(string $coreDir): string
+{
+    $extension = \dirname($coreDir, 2).'/vendor/phpstan/phpstan-symfony/extension.neon';
+
+    return is_file($extension) ? '    - '.$extension : '';
 }
 
 function renderShopwarePhpstanParameters(string $coreDir): string
